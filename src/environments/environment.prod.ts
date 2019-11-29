3266db3cc1a4a100ddc144d5436f2344b1d845f3
Franco Bellucci <belluccifranco@gmail.com>
2019-11-28 22:40:08 -0300

    Quita idEmpresa de los ts de environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://globo-sic-api-production.herokuapp.com',
-  idEmpresa: 1,
+  apiUrl: 'https://globo-sic-api-production.herokuapp.com'
 };
