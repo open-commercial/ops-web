7d9333f055858a2b9def4c80cd7a6a830deeb479
Facundo Sosa López <fssl2004@gmail.com>
2020-01-29 21:25:09 -0300

    Quita archivo de test innecesario. Agrega lógica de returnUrl.

--- src/app/formatters/moment-date-formatter.spec.ts
+++
@@ -1,7 +0,0 @@
-import { MomentDateFormatter } from './moment-date-formatter';
-
-describe('MomentDateFormatter', () => {
-  it('should create an instance', () => {
-    expect(new MomentDateFormatter()).toBeTruthy();
-  });
-});
