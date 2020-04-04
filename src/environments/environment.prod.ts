812605a7dfbaac43382df0664a2cb944844682de
Franco Bellucci <belluccifranco@gmail.com>
2020-04-03 23:18:09 -0300

    Agrega random al appVersion en todos los env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://globo-sic-api-production.herokuapp.com',
-  appVersion: 1,
+  appVersion: Math.floor(Math.random() * 100), // random entre 0 y 100
   appName: 'SIC_OPS_WEB'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080',
-  appVersion: 1,
+  appVersion: Math.floor(Math.random() * 100), // random entre 0 y 100
   appName: 'SIC_OPS_WEB'
 };
