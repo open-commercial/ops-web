0d2575e17e303b5190b8a35d4ccd067548245c83
Facundo Sosa <fssl2004@gmail.com>
2020-07-31 23:50:28 -0300

    - Cambia el formulario de traspaso mas parecido al de pedido.

--- src/app/components/traspaso/traspaso.component.ts
+++ src/app/components/traspaso/traspaso.component.ts
@@ -1,42 +1,16 @@
-import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
-import { Router } from '@angular/router';
-import { Location } from '@angular/common';
-import {
-  AbstractControl,
-  FormArray,
-  FormBuilder,
-  FormControl,
-  FormGroup, ValidationErrors,
-  ValidatorFn,
-  Validators
-} from '@angular/forms';
-import { Sucursal } from '../../models/sucursal';
-import { SucursalesService } from '../../services/sucursales.service';
-import { LoadingOverlayService } from '../../services/loading-overlay.service';
-import { finalize } from 'rxjs/operators';
-import { MensajeService } from '../../services/mensaje.service';
-import { MensajeModalType } from '../mensaje-modal/mensaje-modal.component';
-import { NuevoTraspaso } from '../../models/nuevo-traspaso';
-import { TraspasosService } from '../../services/traspasos.service';
-import { Producto } from '../../models/producto';
-import { CantidadEnSucursal } from '../../models/cantidad-en-sucursal';
-import { ProductoFiltroComponent } from '../producto-filtro/producto-filtro.component';
-
-function nonUniqueIdProductoValidator(): ValidatorFn {
-  return (control: FormArray): {[key: string]: any} | null => {
-    const aux: { [key: number]: number } = {};
-    control.controls.forEach(c => {
-      if (c.value.idProducto) {
-        aux[c.value.idProducto] = aux[c.value.idProducto] ? aux[c.value.idProducto] + 1 : 1;
-      }
-    });
-    const duplicates = [];
-    for (const k in aux) {
-      if (aux.hasOwnProperty(k) && aux[k] > 1) { duplicates.push(k); }
-    }
-    return duplicates.length ? { nonUniqueIdProducto : duplicates } : null;
-  };
-}
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {Location} from '@angular/common';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Sucursal} from '../../models/sucursal';
+import {SucursalesService} from '../../services/sucursales.service';
+import {LoadingOverlayService} from '../../services/loading-overlay.service';
+import {finalize} from 'rxjs/operators';
+import {MensajeService} from '../../services/mensaje.service';
+import {MensajeModalType} from '../mensaje-modal/mensaje-modal.component';
+import {NuevoTraspaso} from '../../models/nuevo-traspaso';
+import {TraspasosService} from '../../services/traspasos.service';
+import {Producto} from '../../models/producto';
 
 @Component({
   selector: 'app-traspaso',
@@ -50,6 +24,9 @@ export class TraspasoComponent implements OnInit {
 
   sucursalesOrigen: Sucursal[] = [];
   sucursalesDestino: Sucursal[] = [];
+
+  cantidadesInicialesPedido: { [idProducto: number]: number } = {};
+  cantidadesActualesPedido: { [idProducto: number]: number } = {};
 
   constructor(private router: Router,
               private location: Location,
@@ -82,38 +59,50 @@ export class TraspasoComponent implements OnInit {
     this.form = this.fb.group({
       idSucursalOrigen: [null, Validators.required],
       idSucursalDestino: [null, Validators.required],
-      idProductoConCantidad: this.fb.array([], [Validators.required, nonUniqueIdProductoValidator()]),
+      productos: this.fb.array([], [Validators.required]),
     });
 
     this.form.get('idSucursalOrigen').valueChanges.subscribe(value => {
       this.form.get('idSucursalDestino').setValue(null);
       this.sucursalesDestino = [];
       if (value) {
-        const aux = this.sucursalesOrigen.filter(s => s.idSucursal !== Number(value));
-        this.sucursalesDestino = aux;
+        this.sucursalesDestino = this.sucursalesOrigen.filter(s => s.idSucursal !== Number(value));
       }
     });
   }
 
   get f() { return this.form.controls; }
 
-  get idProductoConCantidad() {
-    return this.form.get('idProductoConCantidad') as FormArray;
+  get productos() {
+    return this.form.get('productos') as FormArray;
   }
 
-  createIdProductoConCantidadForm() {
+  createProductoForm() {
     return this.fb.group({
-      idProducto: [null, Validators.required],
+      producto: [null, Validators.required],
       cantidad: [1, [Validators.required, Validators.min(1)]],
     });
   }
 
-  addIdProductoConCantidad() {
-    this.idProductoConCantidad.push(this.createIdProductoConCantidadForm());
+  addProductoForm() {
+    this.productos.push(this.createProductoForm());
   }
 
-  removeIdProductoConCantidad(i: number) {
-    this.idProductoConCantidad.removeAt(i);
+  removeProductoForm(i: number) {
+    this.productos.removeAt(i);
+  }
+
+  editarCantidad(i: number) {
+    const pForm = this.productos.at(i);
+    console.log(pForm);
+  }
+
+  selectProducto(p: Producto) {
+    console.log(p);
+  }
+
+  directInputSeleccionProducto(p: Producto) {
+    console.log(p);
   }
 
   submit() {
@@ -139,7 +128,7 @@ export class TraspasoComponent implements OnInit {
     const formValues = this.form.value;
 
     const aux = {};
-    formValues.idProductoConCantidad.forEach(pc => aux[pc.idProducto] = pc.cantidad);
+    formValues.productos.forEach(p => aux[p.producto.idProducto] = p.cantidad);
 
     return {
       idSucursalOrigen: formValues.idSucursalOrigen ? Number(formValues.idSucursalOrigen) : null,
@@ -147,24 +136,4 @@ export class TraspasoComponent implements OnInit {
       idProductoConCantidad: aux,
     };
   }
-
-  /*productoOCantidadChange(productoElementRef: ProductoFiltroComponent, pc: AbstractControl) {
-    const p = productoElementRef.producto;
-    const idSucursalOrigen = this.form.get('idSucursalOrigen').value;
-    const cantControl = pc.get('cantidad');
-
-    if (cantControl.errors && cantControl.errors.insuficiente) {
-      delete cantControl.errors.insuficiente;
-    }
-
-    if (idSucursalOrigen && p) {
-      const aux = p.cantidadEnSucursales.filter(ces => ces.idSucursal === Number(idSucursalOrigen));
-      const cantEnSucursal = aux.length ? aux[0].cantidad : 0;
-      const cantATraspasar = cantControl.value && cantControl.value > 0 ? cantControl.value : 0;
-
-      if (cantEnSucursal < cantATraspasar) {
-        cantControl.setErrors({ insuficiente: { cantATraspasar, cantEnSucursal }});
-      }
-    }
-  }*/
 }
