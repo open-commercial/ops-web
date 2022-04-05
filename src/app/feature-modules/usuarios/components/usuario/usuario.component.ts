970a3b92d97342adcc3e405c93a784f7a4835bb2
Facundo Sosa <fssl2004@gmail.com>
2022-04-05 00:43:18 -0300

    Fix sonarcloud code smells

--- src/app/feature-modules/usuarios/components/usuario/usuario.component.ts
+++ src/app/feature-modules/usuarios/components/usuario/usuario.component.ts
@@ -13,7 +13,6 @@ import { Location } from '@angular/common';
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.component.html',
-  styleUrls: ['./usuario.component.scss']
 })
 export class UsuarioComponent implements OnInit {
   usuario: Usuario;
--- src/app/feature-modules/usuarios/components/usuarios/usuarios.component.ts
+++ src/app/feature-modules/usuarios/components/usuarios/usuarios.component.ts
@@ -20,7 +20,6 @@ import {MensajeModalType} from '../../../../components/mensaje-modal/mensaje-mod
 @Component({
   selector: 'app-usuarios',
   templateUrl: './usuarios.component.html',
-  styleUrls: ['./usuarios.component.scss']
 })
 export class UsuariosComponent extends ListadoDirective implements OnInit {
 
