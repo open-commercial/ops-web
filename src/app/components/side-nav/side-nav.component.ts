4ec72e7665416958893e58bec5fe065b7c150af2
Facundo Sosa <fssl2004@gmail.com>
2022-07-16 00:12:14 -0300

    Refactoriza el side-nav para que sea un accordion, ordena alfabeticamente sus secciones y solo se muestra con el toggle.

--- src/app/components/side-nav/side-nav.component.ts
+++ src/app/components/side-nav/side-nav.component.ts
@@ -1,4 +1,7 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import { filter, Subscription } from 'rxjs';
+import { NavigationEnd, Router } from '@angular/router';
+import { NgbAccordion, NgbAccordionConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Component, EventEmitter, Output, ViewChild, OnDestroy } from '@angular/core';
 import {AuthService} from '../../services/auth.service';
 import {Rol} from '../../models/rol';
 
@@ -7,19 +10,147 @@ import {Rol} from '../../models/rol';
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.scss']
 })
-export class SideNavComponent implements OnInit {
+export class SideNavComponent implements OnDestroy {
   tieneRolAdministrador = false;
   tieneRolAdminOEncargado = false;
   tieneRolVendedor = false;
 
+  menuData = [];
+
   @Output() menuOptionClick = new EventEmitter<void>();
 
-  constructor(private authService: AuthService) { }
+  @ViewChild('accordion') accordion: NgbAccordion;
 
-  ngOnInit() {
+  subscription: Subscription;
+  url='';
+  accordionActiveId='';
+
+  constructor(private authService: AuthService,
+              private router: Router,
+              accordionConfig: NgbAccordionConfig) {
+    accordionConfig.type = 'dark';
+    this.subscription = new Subscription();
+
     this.tieneRolAdministrador = this.authService.userHasAnyOfTheseRoles([Rol.ADMINISTRADOR]);
     this.tieneRolAdminOEncargado = this.authService.userHasAnyOfTheseRoles([Rol.ADMINISTRADOR, Rol.ENCARGADO]);
     this.tieneRolVendedor = this.authService.userHasAnyOfTheseRoles([Rol.VENDEDOR]);
+
+    this.menuData = [
+      {
+        seccion: 'Administración',
+        id: 'administracion',
+        show: this.tieneRolAdminOEncargado,
+        rutas: [
+          { name: 'Cajas', icon: ['fas', 'cash-register'], route: '/cajas', show: true },
+          { name: 'Gastos', icon: ['fas', 'hand-holding-usd'], route: '/gastos', show: true },
+          { name: 'Transportistas', icon: ['fas', 'truck-moving'], route: '/transportistas', show: true },
+          { name: 'Formas de Pago', icon: ['fas', 'money-bill-wave'], route: '/formas-de-pago', show: true },
+          { name: 'Localidades', icon: ['fas', 'map-marked-alt'], route: '/localidades', show: true },
+          { name: 'Rubros', icon: ['fas', 'cubes'], route: '/rubros', show: true },
+        ],
+      },
+      {
+        seccion: 'Compras',
+        id: 'compras',
+        show: this.tieneRolAdminOEncargado,
+        rutas: [
+          { name: 'Facturas', icon: ['fas', 'file-invoice'], route: '/facturas-compra', show: true },
+          { name: 'Proveedores', icon: ['fas', 'truck'], route: '/proveedores', show: true },
+          { name: 'Notas de Crédito', icon: ['fas', 'truck'], route: '/notas-credito-compra', show: true },
+          { name: 'Notas de Débito', icon: ['fas', 'balance-scale-right'], route: '/notas-debito-compra', show: true },
+          { name: 'Recibos', icon: ['fas', 'file-invoice-dollar'], route: '/recibos-compra', show: true },
+        ],
+      },
+      {
+        seccion: 'Sistema',
+        id: 'sistema',
+        show: this.tieneRolAdministrador,
+        rutas: [
+          { name: 'Configuración', icon: ['fas', 'cog'], route: '/configuracion', show: true },
+          { name: 'Sucursales', icon: ['fas', 'store'], route: '/sucursales', show: true },
+          { name: 'Usuarios', icon: ['fas', 'users'], route: '/usuarios', show: true },
+        ],
+      },
+      {
+        seccion: 'Stock',
+        id: 'stock',
+        show: this.tieneRolAdminOEncargado,
+        rutas: [
+          { name: 'Productos', icon: ['fas', 'box-open'], route: '/productos', show: true },
+          { name: 'Traspasos', icon: ['fas', 'exchange-alt'], route: '/traspasos', show: true },
+        ],
+      },
+      {
+        seccion: 'Ventas',
+        id: 'ventas',
+        show: true,
+        rutas: [
+          {
+            name: 'Pedidos',
+            icon: ['fas', 'clipboard-list'],
+            route: '/pedidos',
+            show: true
+          },
+          {
+            name: 'Facturas',
+            icon: ['fas', 'file-invoice'],
+            route: '/facturas-venta',
+            show: this.tieneRolAdminOEncargado || this.tieneRolVendedor
+          },
+          {
+            name: 'Remitos',
+            icon: ['fas', 'file-export'],
+            route: '/remitos',
+            show: this.tieneRolAdminOEncargado || this.tieneRolVendedor
+          },
+          {
+            name: 'Clientes',
+            icon: ['fas', 'portrait'],
+            route: '/clientes',
+            show: this.tieneRolAdminOEncargado
+          },
+          {
+            name: 'Notas de Crédito',
+            icon: ['fas', 'balance-scale-left'],
+            route: '/notas-credito-venta',
+            show: this.tieneRolAdminOEncargado || this.tieneRolVendedor
+          },
+          {
+            name: 'Notas de Débito',
+            icon: ['fas', 'balance-scale-right'],
+            route: '/notas-debito-venta',
+            show: this.tieneRolAdminOEncargado || this.tieneRolVendedor
+          },
+          {
+            name: 'Recibos',
+            icon: ['fas', 'file-invoice-dollar'],
+            route: '/recibos-venta',
+            show: this.tieneRolAdminOEncargado || this.tieneRolVendedor
+          },
+        ],
+      },
+    ];
+
+    this.subscription.add(
+      this.router.events
+        .pipe(filter(event => event instanceof NavigationEnd))
+        .subscribe((event: NavigationEnd) => {
+          let url = '/' + event.urlAfterRedirects.split('/')[1];
+          if (url.indexOf('?') >= 0) {
+            url = url.split('?')[0];
+          }
+
+          const elemento = this.menuData.filter(m => {
+            return m.rutas.filter(r => r.route.indexOf(url) === 0).length > 0;
+          });
+
+          this.accordionActiveId = elemento.length ? elemento[0].id : '';
+        })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   optionClick() {
