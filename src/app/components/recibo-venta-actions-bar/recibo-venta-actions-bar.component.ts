538bcf6d7b5b4e433d0c4d91938e55a05508d2c0
Facundo Sosa <fssl2004@gmail.com>
2023-03-24 09:36:42 -0300

    Creates recibo-actions-bar directive, recibo-compra-actions-bar component and recibo-venta-actions-bar component to reuse their functionality in recibos-venta, recibos-compra and ver-recibo components.

---
+++ src/app/components/recibo-venta-actions-bar/recibo-venta-actions-bar.component.ts
@@ -0,0 +1,91 @@
+import { Cliente } from './../../models/cliente';
+import { NotaDebitoVentaDetalleReciboModalComponent } from './../nota-debito-venta-detalle-recibo-modal/nota-debito-venta-detalle-recibo-modal.component';
+import { NotaDebito } from './../../models/nota';
+import { NuevaNotaDebitoDeRecibo } from './../../models/nueva-nota-debito-de-recibo';
+import { NotaDebitoVentaReciboModalComponent } from './../nota-debito-venta-recibo-modal/nota-debito-venta-recibo-modal.component';
+import { NotasService } from './../../services/notas.service';
+import { ConfiguracionesSucursalService } from './../../services/configuraciones-sucursal.service';
+import { ClientesService } from './../../services/clientes.service';
+import { RecibosService } from './../../services/recibos.service';
+import { AuthService } from './../../services/auth.service';
+import { LoadingOverlayService } from './../../services/loading-overlay.service';
+import { MensajeModalType } from 'src/app/components/mensaje-modal/mensaje-modal.component';
+import { finalize } from 'rxjs/operators';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ReciboActionsBarDirective } from 'src/app/directives/recibo-actions-bar.directive';
+import { MensajeService } from 'src/app/services/mensaje.service';
+import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recibo-venta-actions-bar',
+  templateUrl: './recibo-venta-actions-bar.component.html'
+})
+export class ReciboVentaActionsBarComponent extends ReciboActionsBarDirective {
+
+  constructor(protected router: Router,
+              protected mensajeService: MensajeService,
+              protected loadingOverlayService: LoadingOverlayService,
+              protected authService: AuthService,
+              protected recibosService: RecibosService,
+              private clientesService: ClientesService,
+              private configuracionesSucursalService: ConfiguracionesSucursalService,
+              private notasService: NotasService,
+              private modalService: NgbModal) {
+    super(router, mensajeService, loadingOverlayService, authService, recibosService);
+  }
+
+
+  doCrearNotaDebitoRecibo() {
+    this.loadingOverlayService.activate();
+    this.clientesService.getCliente(this.recibo.idCliente)
+      .pipe(finalize(() => this.loadingOverlayService.deactivate()))
+      .subscribe({
+        next: (c: Cliente) => {
+          const modalRef = this.modalService.open(NotaDebitoVentaReciboModalComponent, { backdrop: 'static' });
+          modalRef.componentInstance.cliente = c;
+          modalRef.componentInstance.idRecibo = this.recibo.idRecibo;
+          modalRef.result
+            .then((data: [NuevaNotaDebitoDeRecibo, NotaDebito]) => {
+              const modalRef2 = this.modalService.open(NotaDebitoVentaDetalleReciboModalComponent, { backdrop: 'static', size: 'lg' });
+              modalRef2.componentInstance.nnddr = data[0];
+              modalRef2.componentInstance.notaDebito = data[1];
+              modalRef2.componentInstance.cliente = c;
+              modalRef2.result.then(
+                (nota: NotaDebito) => this.showNotaCreationSuccessMessage(nota, 'Nota de Débito creada correctamente.', () => {
+                  this.doAutorizar(nota.idNota);
+                }),
+                () => { return; }
+              );
+            }, () => { return; })
+          ;
+        },
+        error: err => this.mensajeService.msg(err.error, MensajeModalType.ERROR),
+      });
+  }
+
+  doAutorizar(idNota: number, callback = () => { return; }) {
+    this.loadingOverlayService.activate();
+    this.configuracionesSucursalService.isFacturaElectronicaHabilitada()
+      .pipe(finalize(() => this.loadingOverlayService.deactivate()))
+      .subscribe({
+        next: habilitada => {
+          if (habilitada) {
+            this.loadingOverlayService.activate();
+            this.notasService.autorizar(idNota)
+              .pipe(finalize(() => this.loadingOverlayService.deactivate()))
+              .subscribe({
+                next: () => this.mensajeService.msg('La Nota fue autorizada por AFIP correctamente!', MensajeModalType.INFO)
+                  .then(() => callback()),
+                error: err => this.mensajeService.msg(err.error, MensajeModalType.ERROR)
+                  .then(() => callback()),
+              })
+            ;
+          } else {
+            this.mensajeService.msg('La funcionalidad de Factura Electronica no se encuentra habilitada.', MensajeModalType.ERROR);
+          }
+        },
+        error: err => this.mensajeService.msg(err.error, MensajeModalType.ERROR),
+      });
+  }
+}
---
+++ src/app/directives/recibo-actions-bar.directive.ts
@@ -0,0 +1,117 @@
+import { finalize } from 'rxjs/operators';
+import { Nota } from './../models/nota';
+import { TipoDeComprobante } from './../models/tipo-de-comprobante';
+import { AuthService } from './../services/auth.service';
+import { Rol } from './../models/rol';
+import { RecibosService } from './../services/recibos.service';
+import { LoadingOverlayService } from './../services/loading-overlay.service';
+import { MensajeModalType } from 'src/app/components/mensaje-modal/mensaje-modal.component';
+import { MensajeService } from 'src/app/services/mensaje.service';
+import { Router } from '@angular/router';
+import { Recibo } from './../models/recibo';
+import { Directive, Input, Output, EventEmitter, OnInit } from '@angular/core';
+
+type ReciboActionButtonName = 'show'|'create-nota-debito'|'delete';
+
+@Directive()
+export abstract class ReciboActionsBarDirective implements OnInit {
+  private pRecibo: Recibo;
+  @Input() set recibo(value: Recibo) { this.pRecibo = value; }
+  get recibo(): Recibo { return this.pRecibo; }
+
+  @Output() afterDelete = new EventEmitter<void>();
+
+  allowedRolesToDelete: Rol[] = [Rol.ADMINISTRADOR, Rol.ENCARGADO];
+  hasRoleToDelete = false;
+
+  allowedRolesToCrearNota: Rol[] = [ Rol.ADMINISTRADOR, Rol.ENCARGADO ];
+  hasRoleToCrearNota = false;
+
+  tiposDeComprobantesParaAutorizacion: TipoDeComprobante[] = [
+    TipoDeComprobante.NOTA_CREDITO_A,
+    TipoDeComprobante.NOTA_CREDITO_B,
+    TipoDeComprobante.NOTA_CREDITO_C,
+    TipoDeComprobante.NOTA_DEBITO_A,
+    TipoDeComprobante.NOTA_DEBITO_B,
+    TipoDeComprobante.NOTA_DEBITO_C,
+  ];
+
+  private pHiddenButtons: ReciboActionButtonName[] = [];
+  @Input() set hiddenButtons(value: ReciboActionButtonName[]) { this.pHiddenButtons = value; }
+  get hiddenButtons(): ReciboActionButtonName[] { return this.pHiddenButtons; }
+
+  hiddenButtonsValues = {
+    'show': false,
+    'delete': false,
+    'create-nota-debito': false,
+  };
+
+  protected constructor(protected router: Router,
+                        protected mensajeService: MensajeService,
+                        protected loadingOverlayService: LoadingOverlayService,
+                        protected authService: AuthService,
+                        protected recibosService: RecibosService) {
+    this.hasRoleToDelete = this.authService.userHasAnyOfTheseRoles(this.allowedRolesToDelete);
+    this.hasRoleToCrearNota = this.authService.userHasAnyOfTheseRoles(this.allowedRolesToCrearNota);
+  }
+
+
+  ngOnInit(): void {
+    Object.keys(this.hiddenButtonsValues).forEach((k: ReciboActionButtonName) => {
+      this.hiddenButtonsValues[k] = this.hiddenButtons.indexOf(k) >= 0;
+    });
+  }
+
+  abstract doCrearNotaDebitoRecibo(): void;
+
+  verRecibo() {
+    this.router.navigate(['/recibos/ver', this.recibo.idRecibo]);
+  }
+
+  eliminarRecibo() {
+    if (!this.hasRoleToDelete) {
+      this.mensajeService.msg('No posee permiso para eliminar recibos.', MensajeModalType.ERROR);
+      return;
+    }
+
+    const msg = 'Esta seguro que desea eliminar el recibo seleccionado?';
+    this.mensajeService.msg(msg, MensajeModalType.CONFIRM).then((result) => {
+      if (result) {
+        this.loadingOverlayService.activate();
+        this.recibosService.eliminarRecibo(this.recibo.idRecibo)
+          .pipe(finalize(() => this.loadingOverlayService.deactivate()))
+          .subscribe({
+            next: () => this.afterDelete.emit(),
+            error: err => {
+              this.loadingOverlayService.deactivate();
+              this.mensajeService.msg(err.error, MensajeModalType.ERROR);
+            }
+          })
+        ;
+      }
+    });
+  }
+
+  crearNotaDeDebitoRecibo() {
+    if (!this.hasRoleToCrearNota) {
+      this.mensajeService.msg('No posee permisos para crear notas.', MensajeModalType.ERROR);
+      return;
+    }
+
+    this.doCrearNotaDebitoRecibo();
+  }
+
+  protected showNotaCreationSuccessMessage(nota: Nota, message: string, callback: () => void = () => { return; }) {
+    if (nota.idNota) {
+      this.mensajeService.msg(message, MensajeModalType.INFO).then(
+        () => {
+          if (this.tiposDeComprobantesParaAutorizacion.indexOf(nota.tipoComprobante) >= 0) {
+            callback();
+          }
+        }
+      );
+    } else {
+      throw new Error('La Nota no posee id');
+    }
+  }
+}
