a7c1353bd8403675da5c33591a5c890dff471de9
rosana <rosmarielnunez@gmail.com>
2024-08-19 17:31:13 +0200

    Add a role the to HomeComponent

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Rol } from 'src/app/models/rol';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   menuOpened = false;
+  rol = Rol;
+  constructor(private router: Router) { }
 
-  toggleMenu() {
+  ngOnInit() {
+    this.redirectBasedOnRole();
+  }
+    toggleMenu() {
     this.menuOpened = !this.menuOpened;
     const elems = document.getElementsByClassName('ops-web-app');
     const appElement = elems.item(0);
@@ -16,6 +22,13 @@ export class HomeComponent {
       appElement.classList.add('menu-opened');
     } else {
       appElement.classList.remove('menu-opened');
+    }
+  }
+  redirectBasedOnRole() {
+    if ( Rol.ADMINISTRADOR || Rol.ENCARGADO) {
+      this.router.navigate(['/dashboard']);
+    } else {
+      this.router.navigate(['/pedidos']);
     }
   }
 }
