f4be0da159933d6ede487e2fdef804715e5b496d
rosana <rosmarielnunez@gmail.com>
2024-11-12 11:14:08 +0100

    Add event detection

--- src/app/services/chart.service.ts
+++ src/app/services/chart.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map, of } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { ChartInterface } from '../models/chart-interface';
 import { environment } from 'src/environments/environment';
 import { SucursalesService } from './sucursales.service';
